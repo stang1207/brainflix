1018fb6cc915e9df67343f86b52b7b41bf98a4bf
Siulok Tang <90478323+stang1207@users.noreply.github.com>
2022-02-23 17:35:18 -0500

    added video-description

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from './components/Header/Header';
 import MainVideo from './components/MainVideo/MainVideo';
+import VideoDescription from './components/VideoDescription/VideoDescription';
 function App() {
   return (
     <>
       <Header />
       <main className="main">
         <MainVideo />
+        <div className="container">
+          <VideoDescription />
+        </div>
       </main>
     </>
   );
