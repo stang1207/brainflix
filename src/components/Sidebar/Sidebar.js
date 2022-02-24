82f23b9a0bc92ead4ff5a91da67e0e201b4216db
Siulok Tang <90478323+stang1207@users.noreply.github.com>
2022-02-24 18:18:58 -0500

    small fixes on padding

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,18 +6,20 @@ const VideoList = ({ currentVideoID, changeCurrentVideo, videos }) => {
     <section className="sidebar">
       <h2 className="sidebar__title">Next videos</h2>
       <ul className="sidebar__video-list">
-        {videos.map((video) => {
-          return (
-            <Video
-              key={video.id}
-              video={video}
-              videoTitle={video.title}
-              videoAuthor={video.channel}
-              imgUrl={video.image}
-              changeCurrentVideo={changeCurrentVideo}
-            />
-          );
-        })}
+        {videos
+          .filter((video) => video.id !== currentVideoID)
+          .map((video) => {
+            return (
+              <Video
+                key={video.id}
+                video={video}
+                videoTitle={video.title}
+                videoAuthor={video.channel}
+                imgUrl={video.image}
+                changeCurrentVideo={changeCurrentVideo}
+              />
+            );
+          })}
       </ul>
     </section>
   );
