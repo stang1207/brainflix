938640082d77e7cf624bc6503e494e31a876bb69
Siulok Tang <90478323+stang1207@users.noreply.github.com>
2022-03-09 16:12:52 -0500

    added error page redirect button

--- client/src/pages/Error/Error.jsx
+++ client/src/pages/Error/Error.jsx
@@ -1,16 +1,20 @@
 import './Error.scss';
 import ErrorImage from '../../assets/images/error.png';
+import { Link } from 'react-router-dom';
 
 const Error = ({ error }) => {
   const { statusCode, errorMessage } = error;
   return (
     <main className="error">
       <img src={ErrorImage} alt="Found an error" className="error__img" />
-      <div className="error__text">
-        <h1 className="error__message">
+      <div className="error__textbox">
+        <p className="error__message">
           <span className="error__code">Error {statusCode}</span>
           {errorMessage}
-        </h1>
+        </p>
+        <Link to="/" className="error__redirect" alt="redirect to homepage">
+          Back to the homepage
+        </Link>
       </div>
     </main>
   );
